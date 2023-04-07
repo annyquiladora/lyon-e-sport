b02ef53c7d756060177211b041e05c7bc47b6f79
Gen Fr.isic-devisic <anny_barrero@hotmail.com>
2023-04-07 11:40:28 +0200

    correction err

--- src/main/java/com/tp/esport/EsportApplication.java
+++ src/main/java/com/tp/esport/EsportApplication.java
@@ -1,4 +1,4 @@
-package com.example.esport;
+package com.tp.esport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tp/esport/EsportApplicationTests.java
+++ src/test/java/com/tp/esport/EsportApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.esport;
+package com.tp.esport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
